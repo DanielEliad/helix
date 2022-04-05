1be6c8accd2c35120811ea695f7b4fbf8f9d2a2c
Blaž Hrastnik <blaz@mxxn.io>
2022-04-05 16:06:13 +0900

    it compiles!

--- helix-view/src/ui/editor.rs
+++ helix-view/src/ui/editor.rs
@@ -55,6 +55,7 @@ impl EditorView {
             keymaps,
             on_next_key: None,
             last_insert: (commands::MappableCommand::normal_mode, Vec::new()),
+            #[cfg(feature = "term")]
             completion: None,
             spinners: ProgressSpinners::default(),
         }
@@ -1175,6 +1176,7 @@ impl Component for EditorView {
                 EventResult::Consumed(None)
             }
             Event::Key(mut key) => {
+                #[cfg(feature = "term")]
                 cx.editor.reset_idle_timer();
                 canonicalize_key(&mut key);
 
@@ -1192,6 +1194,7 @@ impl Component for EditorView {
                         Mode::Insert => {
                             // let completion swallow the event if necessary
                             let mut consumed = false;
+                            #[cfg(feature = "term")]
                             if let Some(completion) = &mut self.completion {
                                 // use a fake context here
                                 let mut cx = Context {
@@ -1212,6 +1215,7 @@ impl Component for EditorView {
 
                             // if completion didn't take the event, we pass it onto commands
                             if !consumed {
+                                #[cfg(feature = "term")]
                                 if let Some(compl) = cx.editor.last_completion.take() {
                                     self.last_insert.1.push(InsertEvent::CompletionApply(compl));
                                 }
@@ -1222,6 +1226,7 @@ impl Component for EditorView {
                                 self.last_insert.1.push(InsertEvent::Key(key));
 
                                 // lastly we recalculate completion
+                                #[cfg(feature = "term")]
                                 if let Some(completion) = &mut self.completion {
                                     completion.update(&mut cx);
                                     if completion.is_empty() {
@@ -1269,6 +1274,7 @@ impl Component for EditorView {
                         };
                         self.last_insert.1.clear();
                     }
+                    #[cfg(feature = "term")]
                     (Mode::Insert, Mode::Normal) => {
                         // if exiting insert mode, remove completion
                         self.completion = None;
